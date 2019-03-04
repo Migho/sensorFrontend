3ed3539c63b5f39fd4cc124f38cde562f78d0d68
Mikko Rinta-Homi <mikkojrh@gmail.com>
2019-03-04 23:48:47 +0200

    Use production backend (Heroku)

--- src/services/data.js
+++ src/services/data.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const source = 'http://localhost:3000/api/sensors'
+const source = 'https://sensorbackend.herokuapp.com/api/sensors'
 
 const getAllSensorData = () => {
   const request = axios.get(source)
